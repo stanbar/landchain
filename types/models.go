f84b59f14a6280dba7a3b689381b79609ee06207
Stanislaw Baranski <stan@stan.bar>
2022-10-16 01:50:51 +0200

    feat: add types

--- types/models.go
+++ types/models.go
@@ -1,24 +1,58 @@
 package types
 
-type NotarialAct struct {
-	ID          string `json:"id"`
-	Type        string `json:"type"`
-	Date        string `json:"date"`
-	Number      string `json:"number"`
-	Description string `json:"description"`
-	Price       string `json:"price"`
+type Owner struct {
+	Share uint `json:"share"`
+	Individual
 }
 
-type Party struct {
+type Mortgage struct {
+	Creditor string `json:"creditor"`
+	Amount   uint   `json:"amount"`
+}
+
+type Land struct {
+	ID       string    `json:"id"`
+	Address  string    `json:"address"`
+	Owners   []Owner   `json:"owners"`
+	Area     uint      `json:"area"`
+	Mortgage *Mortgage `json:"mortgage"`
+}
+
+type NotarialAct struct {
+	LandID       string     `json:"landID"`
+	Notary       Individual `json:"notary"`
+	CreationDate string     `json:"creationDate"`
+	Description  string     `json:"description"`
+	Owner        Individual `json:"owner"`
+}
+
+type State = string
+
+const (
+	Submitted          State = "SUBMITTED"
+	Blocked            State = "BLOCKED"
+	Rejected           State = "REJECTED"
+	Accepted           State = "Accepted"
+	WaitingForPayment  State = "WAITING_FOR_PAYMENT"
+	WaitingForApproval State = "WAITING_FOR_APPROVAL"
+)
+
+type Request struct {
+	LandID       string       `json:"landID"`
+	State        State        `json:"state"`
+	CreationTime string       `json:"creationTime"`
+	Parties      []Individual `json:"parties"`
+	Intermediary Individual   `json:"intermediary"`
+}
+
+type SignedRequest struct {
+	Signatures []string `json:"signatures"`
+	Request
+}
+
+type Individual struct {
 	PESEL     string `json:"pesel"`
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
-	PubKey    string `json:"pubKey"`
-}
-
-type Request struct {
-	CreationTime string  `json:"creationTime"`
-	ID           string  `json:"id"`
-	Parties      []Party `json:"parties"`
-	Signature    string  `json:"signature"`
+	PublicKey string `json:"publicKey"`
 }
