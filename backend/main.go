1c23ffee8fc3ecae127e7fcbc6327e58b764bd06
Stanislaw Baranski <stan@stan.bar>
2022-10-16 04:23:12 +0200

    add backend routes

--- backend/main.go
+++ backend/main.go
@@ -13,5 +13,32 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/getAllRequests", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.POST("/submitRequest", func(c *gin.Context) {
+
+	})
+	r.GET("/getAllRequestsWaitingForApproval", func(c *gin.Context) {
+
+	})
+	r.POST("/acceptRequest", func(c *gin.Context) {
+
+	})
+
+	r.POST("/rejectRequest", func(c *gin.Context) {
+
+	})
+
+	r.GET("/getAllLands", func(c *gin.Context) {
+
+	})
+
+	r.GET("/pay", func(c *gin.Context) {
+
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
