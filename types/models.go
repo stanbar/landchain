f2a6337d05099e213efe924097c9625188a96b95
Stanislaw Baranski <stan@stan.bar>
2022-10-16 02:38:07 +0200

    update models
    Co-authored-by: Grzegorz Baranski <me@gbaranski.com>

--- types/models.go
+++ types/models.go
@@ -11,11 +11,12 @@ type Mortgage struct {
 }
 
 type Land struct {
-	ID       string    `json:"id"`
-	Address  string    `json:"address"`
-	Owners   []Owner   `json:"owners"`
-	Area     uint      `json:"area"`
-	Mortgage *Mortgage `json:"mortgage"`
+	ID          string      `json:"id"`
+	Address     string      `json:"address"`
+	Owners      []Owner     `json:"owners"`
+	Area        uint        `json:"area"`
+	Mortgage    Mortgage    `json:"mortgage"`
+	NotarialAct NotarialAct `json:"notarialAct"`
 }
 
 type NotarialAct struct {
@@ -32,20 +33,22 @@ const (
 	Submitted          State = "SUBMITTED"
 	Blocked            State = "BLOCKED"
 	Rejected           State = "REJECTED"
-	Accepted           State = "Accepted"
+	Accepted           State = "ACCEPTED"
 	WaitingForPayment  State = "WAITING_FOR_PAYMENT"
 	WaitingForApproval State = "WAITING_FOR_APPROVAL"
 )
 
 type Request struct {
-	LandID       string       `json:"landID"`
-	State        State        `json:"state"`
-	CreationTime string       `json:"creationTime"`
-	Parties      []Individual `json:"parties"`
-	Intermediary Individual   `json:"intermediary"`
+	LandID              string       `json:"landID"`
+	State               State        `json:"state"`
+	CreationTime        string       `json:"creationTime"`
+	Parties             []Individual `json:"parties"`
+	Intermediary        Individual   `json:"intermediary"`
+	PreviousNotarialAct NotarialAct  `json:"previousNotarialAct"`
 }
 
 type SignedRequest struct {
+	ID         string   `json:"id"`
 	Signatures []string `json:"signatures"`
 	Request
 }
@@ -55,4 +58,9 @@ type Individual struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
 	PublicKey string `json:"publicKey"`
+}
+
+type Pair[T, U any] struct {
+	First  T
+	Second U
 }
