1c780583d06f3e7803e8fa17bfb737ce3e3877ac
gbaranski <me@gbaranski.com>
2022-10-16 05:09:11 +0200

    feat: add home screen

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,89 +1,21 @@
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://unpkg.com/carbon-components-svelte/css/white.css"
-  />
-</svelte:head>
 <script lang="ts">
-  import { TextInput, Form, TextArea, Grid, Row, Column, Button } from "carbon-components-svelte";
-  import Add from "carbon-icons-svelte/lib/Add.svelte";
-
-  export let request = {
-      landID: '',
-      buyer: {
-          name: '',
-          pesel: '',
-      },
-      seller: {
-          name: '',
-          pesel: '',
-      },
-      intermediary: {
-          name: '',
-          pesel: '',
-      },
-      notarialAct: {
-        notary: {
-          name: '',
-          pesel: '',
-        },
-        description: '',
-      }
-  };
-  
-  const onSubmit = () => {
-    console.log({request});
-
-  };
+  import {  Button } from "carbon-components-svelte";
+    
 </script>
+
+
 <div class="content">
-  <img alt="logo" src="/logo.png" width={500}/>
-  <h3>Podstawowe dane</h3>
-  <TextInput labelText="Numer wpisu w księdze wieczystej" bind:value={request.landID} />
-  <br/>
-  
-  <Grid>
-    <Row>
-      <Column>
-        <h3>Kupujący</h3>
-        <TextInput labelText="Nazwa" bind:value={request.buyer.name} />
-        <br/>
-        <TextInput labelText="Numer PESEL" bind:value={request.buyer.pesel} />
-        <br/><br/>
-      </Column>
-      <Column>
-        <h3>Sprzedający</h3>
-        <TextInput labelText="Nazwa" bind:value={request.seller.name} />
-        <br/>
-        <TextInput labelText="Numer PESEL" bind:value={request.seller.pesel} />
-        <br/><br/>
-      </Column>
-    </Row>
-  </Grid>
+    <img alt="logo" src="/logo.png" width={500}/>
+    <h3>Wybierz co chcesz zrobic</h3>
+    <br/><br/>
+    <Button href="/create">Utwórz nowe zgłoszenie</Button>
+    <br/>
+    <Button href="/unapproved">Zobacz oczekujące zgłoszenia</Button>
+    <br/>
+    <Button href="/lands">Zobacz wszystkie dane księgi wieczystej</Button>
 
-  
-  <h3>Pośrednik</h3>
-  <TextInput labelText="Nazwa" bind:value={request.intermediary.name} />
-  <br/>
-  <TextInput labelText="Numer PESEL" bind:value={request.intermediary.pesel} />
-  <br/><br/>
-
-  <h3>Akt Notarialny</h3>
-  <br/>
-  <TextInput labelText="Nazwa notariusza" bind:value={request.notarialAct.notary.name} />
-  <br/>
-  <TextInput labelText="Numer PESEL notariusza" bind:value={request.notarialAct.notary.pesel} />
-  <br/>
-  <TextArea labelText="Opis" bind:value={request.notarialAct.description}/>
-  <br/><br/>
-
-  <Button icon={Add} on:click={onSubmit}>Dodaj</Button>
 </div>
 <style>
-  :global(body) {
-      background-color: #faf1ec;
-  }
-
   .content {
     margin: 0 auto;
     display: flex;
---
+++ webapp/src/routes/create/+page.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+  import { TextInput, TextArea, Grid, Row, Column, Button } from "carbon-components-svelte";
+  import Add from "carbon-icons-svelte/lib/Add.svelte";
+
+  export let request = {
+      landID: '',
+      buyer: {
+          name: '',
+          pesel: '',
+      },
+      seller: {
+          name: '',
+          pesel: '',
+      },
+      intermediary: {
+          name: '',
+          pesel: '',
+      },
+      notarialAct: {
+        notary: {
+          name: '',
+          pesel: '',
+        },
+        description: '',
+      }
+  };
+  
+  const onSubmit = () => {
+    console.log({request});
+
+  };
+</script>
+<div class="content">
+  <img alt="logo" src="/logo.png" width={500}/>
+  <h3>Podstawowe dane</h3>
+  <TextInput labelText="Numer wpisu w księdze wieczystej" bind:value={request.landID} />
+  <br/>
+  
+  <Grid>
+    <Row>
+      <Column>
+        <h3>Kupujący</h3>
+        <TextInput labelText="Nazwa" bind:value={request.buyer.name} />
+        <br/>
+        <TextInput labelText="Numer PESEL" bind:value={request.buyer.pesel} />
+        <br/><br/>
+      </Column>
+      <Column>
+        <h3>Sprzedający</h3>
+        <TextInput labelText="Nazwa" bind:value={request.seller.name} />
+        <br/>
+        <TextInput labelText="Numer PESEL" bind:value={request.seller.pesel} />
+        <br/><br/>
+      </Column>
+    </Row>
+  </Grid>
+
+  
+  <h3>Pośrednik</h3>
+  <TextInput labelText="Nazwa" bind:value={request.intermediary.name} />
+  <br/>
+  <TextInput labelText="Numer PESEL" bind:value={request.intermediary.pesel} />
+  <br/><br/>
+
+  <h3>Akt Notarialny</h3>
+  <br/>
+  <TextInput labelText="Nazwa notariusza" bind:value={request.notarialAct.notary.name} />
+  <br/>
+  <TextInput labelText="Numer PESEL notariusza" bind:value={request.notarialAct.notary.pesel} />
+  <br/>
+  <TextArea labelText="Opis" bind:value={request.notarialAct.description}/>
+  <br/><br/>
+
+  <Button icon={Add} on:click={onSubmit}>Dodaj</Button>
+</div>
+<style>
+  .content {
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
